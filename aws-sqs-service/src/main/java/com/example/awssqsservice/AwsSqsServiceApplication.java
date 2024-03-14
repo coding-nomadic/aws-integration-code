4c1885d8064eb62768127ee173de13bfa8783314
Tenzin Dawa <tnzngdw@gmail.com>
2024-03-13 22:05:03 -0400

    Add files via upload

---
+++ aws-sqs-service/src/main/java/com/example/awssqsservice/AwsSqsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.awssqsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSqsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSqsServiceApplication.class, args);
+	}
+
+}
---
+++ aws-sqs-service/src/test/java/com/example/awssqsservice/AwsSqsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awssqsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSqsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
